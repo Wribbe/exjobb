16ab23b7bb6a7f6261ea75350faffe68bbe4556f
Stefan Eng <stefan.eng@massive.se>
2020-01-13 16:42:34 +0100

    Handle aborting the test.

--- schema2.sql
+++ schema2.sql
@@ -7,18 +7,26 @@ CREATE TABLE test_user_ids (
 CREATE TABLE test_user (
   id INTEGER PRIMARY KEY
   ,t_created TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f000UTC', 'NOW'))
-  ,str_id TEXT NOT NULL
+  ,str_id TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE test_run (
   id INTEGER PRIMARY KEY
   ,name TEXT NOT NULL
-  ,id_user INTEGER NOT NULL
+  ,id_user TEXT NOT NULL
   ,t_start TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f000UTC', 'NOW'))
   ,t_stop TEXT DEFAULT ""
   ,answer TEXT DEFAULT ""
   ,answer_correct TEXT DEFAULT ""
   ,success BOOLEAN DEFAULT FALSE
   ,vars TEXT
-  ,FOREIGN KEY(id_user) REFERENCES test_user(id)
+  ,FOREIGN KEY(id_user) REFERENCES test_user(str_id)
+);
+
+CREATE TABLE answer (
+  id INTEGER PRIMARY KEY
+  ,str_id TEXT NOT NULL
+  ,question TEXT NOT NULL
+  ,answer TEXT NOT NULL
+  ,FOREIGN KEY(str_id) REFERENCES test_user(str_id)
 );
