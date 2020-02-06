3562e4cc180acf49f6dfd878b175f977ceeaa19b
Stefan Eng <eng_steff@hotmail.com>
2020-02-06 23:56:58 +0100

    Fetch and use variables from database for use in document.

--- csv_to_pdf.py
+++ csv_to_pdf.py
@@ -8,14 +8,22 @@ import matplotlib.pyplot as plt
 def main(path_data, path_out):
   path_data, path_out = [Path(p) for p in [path_data, path_out]]
   data = path_data.read_text().splitlines()
-  code, header, *data = data
+  code, header, title, xlabel, ylabel, legend, *data = data
   code = os.linesep.join([l for l in code.split(';') if l.strip()])
   datasets = []
   exec(code)
   f = plt.figure()
-  for (x, y) in datasets:
-    plt.plot(x, y, "o-")
-  f.set_size_inches(5, 3)
+  lines = []
+  markers = ['o-', 'v--']
+  for (x, y), marker in zip(datasets, markers):
+    lines.append(plt.plot(x, y, marker))
+  f.set_size_inches(6, 3)
+  if legend:
+    plt.legend([l[0] for l in lines], legend.split(','))
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title)
+
   f.savefig(path_out, bbox_inches='tight')
 
 if __name__ == "__main__":
