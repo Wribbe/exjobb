2db4f1402b43e704b03a372b3ac1757be001b383
Stefan Eng <eng_steff@hotmail.com>
2020-02-17 17:37:34 +0100

    Tabulate data, poke around in barh to split bars.

--- csv_to_pdf.py
+++ csv_to_pdf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import sys
 import importlib
-
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -15,16 +14,28 @@ def main(name_module, path_out):
   f = plt.figure()
 
   lines = []
-  markers = ['o-', 'v--',]
+  hashchars = ['\\','/','*']
+  hatchings = [''] + [c*5 for c in hashchars]
+  markers = {
+    'plot': ['o-', 'v--',],
+    'barh': hatchings,
+    'bar': hatchings,
+  }
   method = d.get('method','plot')
-  for (xs, ys), marker in zip(d['data'], markers):
+  kwargs = d.get('kwargs', {})
+  for (xs, ys), marker in zip(d['data'], markers[method]):
+    method_kwargs = kwargs.get(method, {})
     if method in ['barh', 'bar']:
-      lines.append(getattr(plt, method)(xs, ys))
+      lines.append(
+        getattr(plt, method)(
+          xs, ys, hatch=marker, alpha=.99, edgecolor='black', **method_kwargs
+        )
+      )
     else: # Default to a line plot.
       lines.append(plt.plot(xs, ys, marker))
 
   if 'legend' in d:
-    plt.legend([l[0] for l in lines], d['legend'])
+    plt.legend([l[0] for l in lines], d['legend'], **kwargs.get('legend', {}))
   if 'xlabel' in d:
     plt.xlabel(d['xlabel'])
   if 'ylabel' in d:
@@ -41,11 +52,9 @@ def main(name_module, path_out):
     table.set_fontsize(10)
   if 'table_cell_height' in d:
     for cell in table.properties()['child_artists']:
-      print(cell._text.get_position())
       cell.set_height(d['table_cell_height'])
     for cell in table.properties()['child_artists'][::2]:
       pass
-
 
   f.set_size_inches(d.get('size', (6, 3)))
   f.tight_layout()
--- msccls/figures/testsRunPerTask.py
+++ msccls/figures/testsRunPerTask.py
@@ -1,26 +1,61 @@
 from msccls.figures.utils import get_db, dump_vars, as_percent
 def get_data():
+
   db = get_db()
   testruns = db.execute("SELECT * FROM test_run").fetchall()
+  db.close()
+
   test_types = {}
+  tests_per_user = {}
   for run in testruns:
     name = run['name']
     test_types.setdefault(name, 0)
     test_types[name] += 1
 
+    user = run['id_user']
+    tests_per_user.setdefault(user, []).append(run)
+
   test_types = {
-    k:v for k,v in sorted(test_types.items(), key=lambda x: x[1])
+    k:v for k,v in sorted(test_types.items(), key=lambda x: -x[1])
   }
 
-  xs = [t.title() for t in test_types.keys()]
-  ys = list(test_types.values())
+  tests_ten_and_below = {}
+  tests_above_ten = {}
+  for user, runs in tests_per_user.items():
+    if len(runs) > 15:
+      dic = tests_above_ten
+    else:
+      dic = tests_ten_and_below
+    for run in runs:
+      name = run['name']
+      dic.setdefault(name, 0)
+      dic[name] += 1
 
-  db.close()
+  labels = list(test_types.keys())
+  dics = [test_types, tests_ten_and_below, tests_above_ten]
+  height = 0.9/len(dics)
 
+  data = []
+  for i, dic in enumerate(dics):
+    ys = [dic[l] for l in labels]
+    data.append(([height*-i-ii for ii,_ in enumerate(ys)], ys))
+
+
+  print(data)
   return {
-    'data': [(xs, ys)],
+    'data': data,
     'method': 'barh',
-#    'xticks': [0,50,100, 150,]
+    'yticks': [-x-height/2 for x in range(len(labels))],
     'ylabel': "Task type",
     'xlabel': "Number of test run",
+    'legend': ['#$r>0$', '#$r\leq15$', '#$r\geq16$'],
+    'kwargs': {
+      'legend': {
+        'title': "Runs/User:",
+      },
+      'barh': {
+        'height': height,
+        'tick_label': [l.title() for l in labels],
+      }
+    }
   }
