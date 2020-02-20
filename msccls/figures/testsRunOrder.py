8326faf1edbcfbf3205717e31cc028bc04b20eca
Stefan Eng <eng_steff@hotmail.com>
2020-02-20 10:07:52 +0100

    Add and track figure generators.

---
+++ msccls/figures/testsRunOrder.py
@@ -0,0 +1,73 @@
+from msccls.figures.utils import \
+  get_db, dump_vars, as_percent, iter_markers, iter_colors
+
+def get_data():
+
+  db = get_db()
+  testruns = db.execute("SELECT * FROM test_run WHERE t_stop").fetchall()
+  db.close()
+
+  tests_per_user = {}
+  for run in testruns:
+    user = run['id_user']
+    tests_per_user.setdefault(user, []).append(run['name'])
+
+  tests_per_user = [(u,n) for u,n in tests_per_user.items() if len(n) < 16]
+  types = {}
+  data, widths, xticks, xlabels, colors, markings = [], [], [], [], [], []
+  group_width = 0.8
+
+  for _, names in tests_per_user:
+    for x, name in enumerate(names, start=1):
+      types.setdefault(x, {})
+      types[x].setdefault(name, 0)
+      types[x][name] += 1
+
+  order = [
+    'employee hours',
+    'team workload',
+    'task dependencies',
+    'team performance'
+  ]
+
+  mappings_style = dict(zip(order, zip(iter_colors, iter_markers)))
+  handlers_for_legend = {}
+  num_bar = 0
+  for x, dict_types in types.items():
+    xticks.append(x+group_width/2)
+    xlabels.append(x)
+    total = sum(dict_types.values())
+    i = 0
+    for name in order:
+      value = dict_types.get(name)
+      if not value:
+        continue
+      widths.append(group_width/len(dict_types))
+      color, marker = mappings_style[name]
+      colors.append(color)
+      markers.append(marker)
+      handlers_for_legend[name] = num_bar
+      dx = widths[-1]
+      bx = x+dx/2+dx*i
+      by = (value/total*100)
+      data.append((bx, by))
+      num_bar += 1
+      i += 1
+
+  return {
+    'data': data,
+    'method': 'bar',
+    'xticks': (xticks, xlabels),
+    'yticks': range(110)[::10],
+    'widths': widths,
+    'markers': markers,
+    'colors': colors,
+    'legend': handlers_for_legend,
+    'kwargs': {
+      'legend': {
+        'fontsize': 'small',
+      },
+    },
+   'ylabel': "Type percentage per task-run",
+   'xlabel': "Test index, from the first test and onwards",
+  }
---
+++ msccls/figures/testsRunOrderOutliers.py
@@ -0,0 +1,78 @@
+from msccls.figures.utils import \
+  get_db, dump_vars, as_percent, iter_markers, iter_colors
+
+import matplotlib as plt
+import numpy as np
+
+def get_data():
+
+  db = get_db()
+  testruns = db.execute("SELECT * FROM test_run WHERE t_stop").fetchall()
+  db.close()
+
+  tests_per_user = {}
+  for run in testruns:
+    user = run['id_user']
+    tests_per_user.setdefault(user, []).append(run['name'])
+
+  tests_per_user = [(u,n) for u,n in tests_per_user.items() if len(n) > 15]
+  types = {}
+  data, widths, xticks, xlabels, colors, markings = [], [], [], [], [], []
+
+  group_width = 0.8
+
+  for _, names in tests_per_user:
+    for x, name in enumerate(names, start=1):
+      types.setdefault(x, {})
+      types[x].setdefault(name, 0)
+      types[x][name] += 1
+
+  order = [
+    'employee hours',
+    'team workload',
+    'task dependencies',
+    'team performance'
+  ]
+
+  mappings_style = dict(zip(order, zip(iter_colors, iter_markers)))
+  handlers_for_legend = {n: 0 for n in order}
+
+  bar_num = 0
+  for x, dict_types in types.items():
+    total = sum(dict_types.values())
+    sum_prev = 0
+    bars_at_x = []
+    for name in order:
+      value = dict_types.get(name, 0)/total*100
+      bars_at_x.append((name, value+sum_prev))
+      sum_prev += value
+
+    # Print in inverted order.
+    for name, value in reversed(bars_at_x):
+      if value == 0:
+        continue
+      handlers_for_legend[name] = bar_num
+      color, marking = mappings_style[name]
+      colors.append(color)
+      markers.append(marking)
+      bar_num += 1
+      data.append((x, value))
+
+  return {
+    'data': data,
+    'method': 'bar',
+    'legend': handlers_for_legend,
+    'markers': markers,
+    'colors': colors,
+    'yticks': range(110)[::10],
+    'xticks': [1] + list(range(10, 110)[::10]),
+    'ylabel': "Stacked task-type percentages",
+    'xlabel': "Test run number",
+    'kwargs': {
+      'legend': {
+        'bbox_to_anchor': (1.0, 0.5),
+        'loc': 'center right',
+        'fontsize': 'small',
+      }
+    }
+  }
