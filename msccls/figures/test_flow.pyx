5d2ffcf7569f8ae56b70ca1411fa71f130a60cf1
Stefan Eng <eng_steff@hotmail.com>
2020-04-14 12:38:28 +0200

    Commitish~

---
+++ msccls/figures/test_flow.pyx
@@ -0,0 +1,55 @@
+from pyx import *
+from pyx.connector import arc, curve, twolines, line
+import sys
+
+unit.set(uscale=2)
+
+c = canvas.canvas()
+
+textattrs = [text.halign.center, text.vshift.middlezero]
+E = text.text(-1*2.5, -1*0.5, r"""\bf Information and consent""", textattrs)
+D = text.text(-0.3, -1*0.5, r"\bf Pre-survey", textattrs)
+C = text.text(1*1.4, -1*0.5, r"\bf Test-run \#1", textattrs)
+F = text.text(1*1.4, -2*0.5, r"\bf Test-run \#2", textattrs)
+G = text.text(-0.3, -2*0.5, r"\bf Test-run \#3", textattrs)
+H = text.text(-1*2.0, -2*0.5, r"\bf Test-run \#4", textattrs)
+J = text.text(-1*2.0, -3*0.5, r"\bf Test-run \#5..n", textattrs)
+K = text.text(-0.3, -3*0.5, r"\bf Post-survey", textattrs)
+
+c.stroke(
+  path.circle(0, 0, 0.1),
+  [
+    style.linestyle.dashed,
+    deco.earrow(),
+    trafo.rotate(210),
+    trafo.translate(-2.82, -1.51)
+  ]
+)
+
+for X in [C, D, E, F, G, H, J, K]:
+    c.draw(X.bbox().enlarged(0.05).path(),
+        [deco.stroked()])
+    c.insert(X)
+
+
+for X,Y in [[C, D], [D, E], [F, C], [G, F], [H, G], [J, H], [K, J]]:
+    c.stroke(arc(Y, X, boxdists=[0.10, 0.10], relangle=0), [color.rgb.black, deco.earrow.normal])
+
+#c.stroke(arc(E, D, boxdists=[0.15,0.15], relangle=0), [color.rgb.black, deco.earrow.normal])
+##c.stroke(curve(D, B, boxdists=[0.2, 0.2], relangle1=-45, relangle2=-45, relbulge=0.8),
+#         [color.rgb.blue, deco.earrow.normal])
+
+#E = text.text(2*2.5/2, -1*0.6-1*0.1, r"""\bf Initial Consent \& Information""", textattrs)
+#D = text.text(2*2.5/2, -2*0.6-1*0.1, r"\bf Pre-survey complete", textattrs)
+#C = text.text(2*2.5/2, -3*0.6-1*0.1, r"\bf Post-survey complete", textattrs)
+
+#for X in [C, D, E]:
+#    c.draw(X.bbox().enlarged(0.05).path(),
+#        [deco.stroked()])
+#    c.insert(X)
+#
+#for X,Y in [[C, D], [D, E]]:
+#    c.stroke(arc(Y, X, boxdists=[0.20, 0.10], relangle=0), [color.rgb.black, deco.earrow.normal])
+#
+
+c.writePDFfile(sys.argv[1])
